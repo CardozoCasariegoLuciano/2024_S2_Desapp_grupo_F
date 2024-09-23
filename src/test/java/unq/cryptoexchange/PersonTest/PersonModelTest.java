fae60b74bd80e1099a5e40307b4f437e6369b618
Luciano Cardozo Casariego <lucianocardozocasariego@gmail.com>
2024-09-23 20:26:51 -0300

    fix sonnar issues

--- src/test/java/unq/cryptoexchange/PersonTest/PersonModelTest.java
+++ src/test/java/unq/cryptoexchange/PersonTest/PersonModelTest.java
@@ -3,20 +3,19 @@ package unq.cryptoexchange.PersonTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import unq.cryptoexchange.dto.request.PersonRegistrationDto;
 import unq.cryptoexchange.models.ExchangeAttempt;
 import unq.cryptoexchange.models.Person;
 import unq.cryptoexchange.models.enums.OperationType;
 
 @SpringBootTest
-public class PersonModelTest {
+class PersonModelTest {
 
     @Test
     void test_01_APersonCanCreateAExchangeAttempt() {
         Person person = new Person();
         ExchangeAttempt attemp = person.createAttempt("test",23,22.4f, OperationType.BUY);
 
-        Assertions.assertEquals(attemp.getCrypto(), "test");
+        Assertions.assertEquals("test", attemp.getCrypto());
 
     }
 }
