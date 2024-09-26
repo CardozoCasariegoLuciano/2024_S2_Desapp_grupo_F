616be8a7d3fda555688777b60f52f04ea497b16d
Luciano Cardozo Casariego <lucianocardozocasariego@gmail.com>
2024-09-26 19:21:24 -0300

    update swagger version

--- src/main/java/unq/cryptoexchange/configuration/SwaggerConfig.java
+++ src/main/java/unq/cryptoexchange/configuration/SwaggerConfig.java
@@ -11,7 +11,7 @@ public class SwaggerConfig {
     public OpenAPI openAPI() {
         return new OpenAPI().info(new Info()
                 .title("Crypto Exchange")
-                .version("0.0.1")
+                .version("0.0.2")
                 .description("TP Desarrollo de aplicaciones 2024 S2"));
     }
 
